50bc5df8758d95198fbb087e75d6fa491fb8cbba
pratik20gb <pratik20gb@gmail.com>
2026-03-04 23:08:38 +0530

    compelted exercise 9.5 which is stretch of 9.3

--- Chapter9/exercise9_3.py
+++ Chapter9/exercise9_3.py
@@ -8,16 +8,47 @@ Create several instances representing different users, and call both methods for
 
 """
 
+# class User:
+#     def __init__(self,first_name,last_name):
+#         self.first_name = first_name
+#         self.last_name = last_name
+        
+#     def describe_user(self):
+#         print(f"the user is {self.first_name}{self.last_name}")
+#     def greet_user(self):
+#         print(f"Hi {self.first_name} u doing good bud")
+# user1 = User('allye','coder')
+# user1.describe_user()
+# user1.greet_user()
+
+
+
+"""
+9-5. Login Attempts: Add an attribute called login_attempts to your User class
+from Exercise 9-3 (page 162). Write a method called increment_login_attempts()
+that increments the value of login_attempts by 1. Write another method called
+reset_login_attempts() that resets the value of login_attempts to 0.
+Make an instance of the User class and call increment_login_attempts()
+several times. Print the value of login_attempts to make sure it was incremented
+properly, and then call reset_login_attempts(). Print login_attempts again to
+make sure it was reset to 0.
+"""
 class User:
-    def __init__(self,first_name,last_name):
+    def __init__(self,first_name,last_name,login_attempts):
         self.first_name = first_name
         self.last_name = last_name
+        self.login_attempts = 0
         
     def describe_user(self):
-        print(f"the user is {self.first_name}{self.last_name}")
+        print(f"the user is {self.first_name} {self.last_name} : no of loggins {self.login_attempts}")
     def greet_user(self):
         print(f"Hi {self.first_name} u doing good bud")
-user1 = User('allye','coder')
-user1.describe_user()
-user1.greet_user()
-
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+    def reset_login_attempt(self):
+        self.login_attempts = 0
+user2 = User('alley','coder','1')
+user2.increment_login_attempts()
+user2.increment_login_attempts()
+user2.increment_login_attempts()
+user2.describe_user()
