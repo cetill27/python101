fdc8ac878311dcea593bb52ce9edcf8832b872de
pratik20gb <pratik20gb@gmail.com>
2026-03-05 17:15:58 +0530

    module export

---
+++ Chapter9/Inheritance/my_car.py
@@ -0,0 +1,16 @@
+# import sys
+# import os
+
+# sys.path.append(os.path.abspath(".."))
+
+
+# from CarClass import Car
+from elctricCar import Car,Battery
+
+# a_car = Car('audi','a3',2024)
+# print(a_car.get_descriptive_name())
+# a_car.odomoter_reading =23
+# a_car.read_odometer()
+
+my_mustang = Car('ford','mustang',2024)
+print(my_mustang.get_descriptive_name())
